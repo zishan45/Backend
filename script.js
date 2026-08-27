c06c84255670889ca629039e347c60a477385cbc
Zishan Ahmed <zishanahmed.official@gmail.com>
2026-08-27 10:38:30 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+// let n= 5;
+
+// for (let i=0; i<n; i++){
+//     console.log("Hello", i);
+// }
+
+// console.log(process.argv);
+
+// let args = process.argv;
+
+// for (let i=2; i<args.length; i++){
+//     console.log("Hello to,", args[i]);
+// }
+
+// let data = require('./math');
+// console.log(data);
+// console.log(data.sum(2,3));
+
+let fruits_info = require("./Fruits");
+
+console.log(fruits_info);
