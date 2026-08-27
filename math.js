34ad4cbafa4bc1245b9dfe6f512fc620d6853855
Zishan Ahmed <zishanahmed.official@gmail.com>
2026-08-27 10:37:55 +0530

    math.js

---
+++ math.js
@@ -0,0 +1,15 @@
+const sum = (a,b) => a+b;
+const mul = (a,b) => a*b;
+const mod = (a,b) => a%b;
+const g = 9.88;
+const pie = 3.14;
+
+let obj = {
+    sum: sum,
+    mul: mul,
+    mod: mod,
+    gravity: g,
+    pie: pie,
+}
+
+module.exports = obj;
