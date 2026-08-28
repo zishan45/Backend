c5cc7258e7563060f51ed0ef871f31e96d7b2fe4
Zishan Ahmed <zishanahmed.official@gmail.com>
2026-08-28 10:05:48 +0530

    Template:EJS

---
+++ EJS/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+
+const port = 8080;
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+    // res.send("This is home");
+    res.render("home.ejs");
+
+});
+
+app.get("/rolldice", (req, res) => {
+    let num = Math.floor(Math.random() * 6 + 1 );
+    res.render("rolldice.ejs", {diceVal: num});
+    res.render("rolldice.ejs");
+});
+
+app.get("/instagram/:username", (req, res) => {
+    let {username} = req.params;
+    let followers = ["rohit", "kohli", "jadeja", "rahul", "bumrah"];
+    res.render("instagram.ejs", {username, followers});
+
+});
+
+app.listen(port, () => {
+    console.log(`listening to port ${port}.`);
+});
