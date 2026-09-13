19cc5261764abc4d20cbf5e787f11b361d4feda0
Zishan Ahmed <zishanahmed.official@gmail.com>
2026-09-13 13:32:00 +0530

    Middleware and Authentication

---
+++ Middleware/app.js
@@ -0,0 +1,32 @@
+const express= require("express");
+const app = express();
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`listening to middleware port ${port}`);
+});
+
+// middleware -> response sent here, then later part og app.get will not be sent to server or client
+
+app.use((req, res, next) => {     // here we have not given route so it takes by default "/"
+    console.log("Hii i am 1st middleware");
+    next();
+});
+
+app.get("/", (req, res) => {
+    res.send("This is a home page");
+});
+
+app.get("/random", (req, res) => {
+    res.send("this is a random page");
+});
+
+app.use("/api", (req, res, next) => {
+    let {token} = req.query;
+    if (token === "apitoken"){
+        res.send("Here is your data");
+        next();
+    }
+    res.send("Access Denied, API Token doesn't match");
+});
+
